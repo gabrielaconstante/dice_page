9ddc96a01de777586e246e5e47bfac672ff0fad4
gabrielaconstante <gabrielaconstante04@gmail.com>
2023-09-06 19:54:48 -0300

    Padding

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,20 @@ class DicePage extends StatelessWidget {
   const DicePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 2,
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // flex: 2,
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('images/dice1.png')),
+          ),
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+        ],
+      ),
     );
   }
 }
